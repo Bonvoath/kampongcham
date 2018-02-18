22b5b8fdc7c7ad1a3648571ae52f1ded0ec52680
Bonvoath <bonvoathtanson@gmail.com>
2018-02-18 22:17:10 +0700

    update menu

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -2,26 +2,26 @@
 @section('content')
     <div class="row">
         <div class="col-lg-4 col-sm-4">
-            <div class="card" style="margin-bottom:1px;">
-                <div class="card-header">Pages <span class="pull-right clickable panel-collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
+            <div class="card" style="margin-bottom:2px;">
+                <div class="card-header">Pages <span class="circle clickable pull-right"><i class="fa fa-chevron-down" aria-hidden="true"></i></span></div>
                 <div class="card-block">
-                
+                    
                 </div>
             </div>
-            <div class="card" style="margin-bottom:1px;">
-                <div class="card-header">Posts <span class="pull-right clickable panel-collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
+            <div class="card" style="margin-bottom:2px;">
+                <div class="card-header">Posts <span class="circle pull-right clickable collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
                 <div class="card-block" style="display:none;">
                 
                 </div>
             </div>
-            <div class="card" style="margin-bottom:1px;">
-                <div class="card-header">Categories <span class="pull-right clickable panel-collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
+            <div class="card" style="margin-bottom:2px;">
+                <div class="card-header">Categories <span class="circle pull-right clickable collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
                 <div class="card-block" style="display:none;">
                 
                 </div>
             </div>
             <div class="card">
-                <div class="card-header">Custom Link <span class="pull-right clickable panel-collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
+                <div class="card-header">Custom Link <span class="circle pull-right clickable collapsed"><i class="fa fa-chevron-right" aria-hidden="true"></i></span></div>
                 <div class="card-block" style="display:none;">
                 
                 </div>
@@ -42,4 +42,20 @@
             </div>
         </div>
     </div>
+@endsection
+@section('js')
+    <script>
+        $('body').on('click', '.card span.clickable', function(e){
+            var $this = $(this);
+            if(!$this.hasClass('collapsed')) {
+                $this.parents('.card').find('.card-block').slideUp();
+                $this.addClass('collapsed');
+                $this.find('i').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+            } else {
+                $this.parents('.card').find('.card-block').slideDown();
+                $this.removeClass('collapsed');
+                $this.find('i').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+            }
+        });
+    </script>
 @endsection
