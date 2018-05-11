4c126b3bac2c23e5d26a9d9c67b09fd5c124fe1f
Bonvoath <bonvoathtanson@gmail.com>
2018-05-11 11:43:40 +0700

    comment

--- resources/views/mobiles/detail.blade.php
+++ resources/views/mobiles/detail.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.mobile')
 @section('content')
     <div>
-        <h4 style="line-height:37px;">{{$detail->title}}</h4><br>
+        <div style="font-size: 16px; font-weight: 600; line-height:24px;">{{$detail->title}}</div>
         <small style="color: gray;"><img src="{{asset('front/img/date.png')}}" alt="date">  : {{$detail->create_at}}</small>
         <hr>
         <div id="box_content">
