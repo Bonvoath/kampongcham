a3c828cd68479baab14bc556629a5a786ff101e4
Bonvoath <bonvoathtanson@gmail.com>
2018-06-05 14:50:27 +0700

    update api

--- app/Http/Controllers/ApiV1Controller.php
+++ app/Http/Controllers/ApiV1Controller.php
@@ -10,12 +10,21 @@ use Response;
 class ApiV1Controller extends Controller
 {
     public function api_get_all_post(){
+        $page = 1;
+        $limit = 25;
         $data['posts'] = DB::table('posts')
+                        ->select('id','title','feature_image','create_at')
                         ->orderBy('id', 'desc')
                         ->where('post_type', 'post')
                         ->where('active', 1)
-                        ->paginate(25);
-                        
+                        ->paginate($limit);
+
+        foreach($data['posts'] as $ret)
+        {
+            unset($ret->description);
+            $ret->feature_image = url('/uploads/posts/250x250/'.$ret->feature_image);
+        }
+
         return Response::json($data);
     }
 }
